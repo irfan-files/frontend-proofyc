11feceef701b1dfdab0dcae4680bb8c4a10f8bf5
akbarwijayaa <jimsaber02@gmail.com>
2024-10-10 18:32:56 +0700

    fix: remove button switch network

--- src/components/ConnectWalletButton.js
+++ src/components/ConnectWalletButton.js
@@ -60,10 +60,6 @@ function ConnectWalletButton() {
             <button type="button" className='bg-blue-500 text-white px-6 py-3 rounded-full hover:bg-blue-600' onClick={() => disconnect()}>
                 Disconnect
             </button>
-                  <button type="button" className='bg-blue-500 text-white px-6 py-3 rounded-full hover:bg-blue-600' onClick={() => switchChain({ chainId: REQUIRED_CHAIN.id })}>
-                    Switch Network
-                  </button>
-
             <br />
             <div className="flex flex-col items-center">
             <h1 className="text-3xl font-bold mb-6">HealthyFood NFT</h1>
