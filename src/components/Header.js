d5ff3db0da0a0d7b21c5bed29e79e9c43de6b040
irfan-files <vfx55782@gmail.com>
2024-10-12 00:59:38 +0700

    fix header

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,6 @@ const Header = () => {
     address,
     handleConnect,
     handleDisconnect,
-    metamaskConnector,
     coinbaseConnector,
   } = useWallet();
 
@@ -25,8 +24,7 @@ const Header = () => {
       return (
         <>
           <button
-            className="px-4 py-2 bg-red-500 text-white font-semibold rounded-lg
-            "
+            className="transition ease-in-out delay-150 px-4 py-2 bg-red-500 text-white font-semibold rounded-lg"
             onClick={handleDisconnect}
           >
             Disconnect
@@ -38,7 +36,7 @@ const Header = () => {
     return (
       <button
         type="button"
-        className="px-4 py-2 bg-lime-400 text-slate-700 font-semibold rounded-lg"
+        className="transition ease-in-out delay-150 px-4 py-2 bg-lime-400 text-slate-700 font-semibold rounded-lg"
         onClick={openModal}
       >
         Connect Wallet
@@ -48,40 +46,17 @@ const Header = () => {
 
   return (
     <header className="w-full bg-gray-900 border-b border-neutral-200 p-4 font-coinbase">
-      <Link to="/">
-        <img
-          className="w-28"
-          src="https://via.placeholder.com/117x32"
-          alt="Logo"
-        />
-
-        <div className="text-yellow-500 text-xl font-bold">
-          <div>CREATOR</div>
-          <div>BEAM</div>
-        </div>
-      </Link>
       <div className="max-w-7xl mx-auto flex justify-between items-center">
-        {/* Logo */}
+        {/* Left side - Logo */}
         <Link to="/">
-          <img
-            className="w-28"
-            src="https://via.placeholder.com/117x32"
-            alt="Logo"
-          />
+          <div className="text-yellow-500 text-xl font-bold">
+            <div>CREATOR</div>
+            <div>BEAM</div>``
+          </div>
         </Link>
 
-        {/* Navigation */}
-        {/* <nav className="flex gap-4">
-          <Link to="/dashboard" className="text-white text-lg font-bold">
-            Dashboard
-          </Link>
-          <Link to="/marketplace" className="text-gray-400 text-lg">
-            Marketplace
-          </Link>
-        </nav> */}
-
-        {/* Wallet and Profile */}
-        <div className="flex items-center gap-4">
+        {/* Right side - Wallet and Profile */}
+        <div className="ml-auto flex items-center gap-4">
           {renderWalletButton()}
           <div className="w-10 h-10 rounded-full bg-gray-800"></div>
         </div>
